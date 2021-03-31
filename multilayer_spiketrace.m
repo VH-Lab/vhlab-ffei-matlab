914ec588c1592e81a90a121ac008cdee88020596
Alex Cao <AlexCao@Alexs-Air-2.fios-router.home>
2021-03-31 12:49:53 -0400

    started on Figure 5

---
+++ multilayer_spiketrace.m
@@ -0,0 +1,8 @@
+% MULTILAYER_SPIKETRACE: Generates figures comparing spiking behavior of 
+% 4-layer triad synapse circuit models (Figure 5).
+% NOTE: this one is a bit trickier to adapt: run_triad_model.m provides
+% randomized inputs (background and feed-forward) during each trial, but in
+% the spike traces shown in Figure 5A, all models (3 trials) recieve the
+% same inputs. Perhaps provide custom inputs for both background noise and
+% signal, and generate all inputs in this script? For now, plotting of the
+% outputs from each layer also occurs in this script.
