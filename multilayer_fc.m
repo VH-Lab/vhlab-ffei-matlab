018651efdfcd4f94225746a1780a1abbdc54764c
Alex Cao <AlexCao@alexs-air-2.lan>
2021-04-07 09:55:21 -0400

    Figure 5B-D necessary functions/scripts

---
+++ multilayer_fc.m
@@ -0,0 +1,3 @@
+% MULTILAYER_FC: Generates figures comparing final output power of 
+% 4-layer triad synapse circuit models (Figure 5).
+
--- triad_multilayer.m
+++ triad_multilayer.m
@@ -2,3 +2,16 @@
 %   Comparison of 4-layer retina-LGN 1-to-1 circuits, exhibiting 1) noise 
 %   only, 2)feedforward-E transmission only or 3) feedforward-E/I
 %   transmission (triad synapse).
+
+clear;
+% Generate spike traces (panel A)
+disp('Generating spike traces...')
+multilayer_spiketrace();
+
+clear;
+% Generate FC plots (panel B-E)
+disp('Generating FC figures...')
+multilayer_fc();
+
+
+
