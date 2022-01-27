5c251be9d7052d6d9c709f0354f939c78135c636
Alex Cao <AlexCao@Alexs-MacBook-Air-2.local>
2022-01-27 13:21:38 -0500

    Figure 8, 10 functions

---
+++ cortex_ampa.m
@@ -0,0 +1,5 @@
+% CORTEX_AMPA: Generates the panels of Figure 10.
+%   Comparison of cortical 100-to-1 circuits, exhibiting 1) FFE projections
+%   to cortical cell only, 2) FFE projections to inhibitory interneuron
+%   and cortical cell + FFI projection from interneuron to cortical cell,
+%   or 3) Same as 2, but with AMPA only inputs to inhibitory interneuron
---
+++ triad_depression.m
@@ -0,0 +1,5 @@
+% TRIAD_DEPRESSION: Generates the panels of Figure 8.
+%   Comparison of retina-LGN 1-to-1 circuits, exhibiting postsynaptic 1)
+%   AMPA+NMDA channels with synaptic depression, or 2)
+%   AMPA+NMDA+GABAA+GABAB channels with synaptic depression.
+
